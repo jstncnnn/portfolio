3ed614e036bac15c25442ec528bf1afee33694f3
Justin <jjustincunanan@yahoo.com>
2023-10-24 13:29:40 +0800

    not yet responsive

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,18 +1,45 @@
 import "./ContactMe.css";
 
 import React from "react";
+import { AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
+import {GrMapLocation} from "react-icons/gr";
+
 
 function ContactMe() {
   return (
     <div className="contact-container">
-      <div>
-      <h3>Contact Me!</h3>
-      <p>hello</p></div>
-      <div>
+      <div className="contact-content">
+      <h3>CONTACT</h3>
+      <p>Hit me up!</p><br/><br/><br/>
+      <div className="contact-details">
+      <div className="contact-iconbox">
+        <AiOutlinePhone className="contact-icons" size={30} style={{ position: "relative", top: "8px"}} />
+        <div className="contact-info">
+          <h3>Contact Number</h3>
+          <p>0970 646 5775</p>
+        </div>        </div>
+      
+      <div className="contact-iconbox"> 
+      <GrMapLocation className="contact-icons" size={30} style={{ position: "relative", top: "8px" }} />
+      <div className="contact-info">
+          <h3>Location</h3>
+          <p>Imus, Cavite</p>
+        </div>
+        </div>
+        <div className="contact-iconbox"> 
+      <AiOutlineMail className="contact-icons" size={30} style={{ position: "relative", top: "8px" }} />
+      <div className="contact-info">
+          <h3>E-mail</h3>
+          <p>jjustincunanan@gmail.com</p>
+        </div>
+        </div>
+
+      </div>
+      </div>
       </div>
 
-    </div>
   );
 }
 
 export default ContactMe;
+
--- src/components/Services.js
+++ src/components/Services.js
@@ -5,7 +5,7 @@ import React from "react";
 function Services() {
   return (
     <div className="services-container">
-      <h1 className="h1-services"> Services </h1>
+      <h1 className="h1-services"> Projects </h1>
     </div>
   );
 }
